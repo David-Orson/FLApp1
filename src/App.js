9a5f3f92743706d8b9837d099501c155eab48149
David Orson <onlyorson@outlook.com>
2020-08-18 22:06:00 +0100

    Reach Router and App structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Router } from "@reach/router";
+
+import Navbar from "./components/Navbar";
+
+import Sync from "./pages/Sync";
+import Products from "./pages/Products";
+import Categories from "./pages/Categories";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="ui container">
+        <Router>
+          <Sync path="/" />
+          <Products path="/products" />
+          <Categories path="/categories" />
+          <Quiz
+            path="/quiz"
+            userAnswers={userAnswers}
+            setAnswers={setAnswers}
+          />
+          <Result
+            path="/result"
+            userAnswers={userAnswers}
+            resetAnswers={resetAnswers}
+          />
+        </Router>
+      </div>
+    </div>
+  );
 };
 
 export default App;
