1c6c9e118de590888c48b47ab8baa6f9f18b27ae
David Orson <onlyorson@outlook.com>
2020-08-20 00:06:39 +0100

    User removed from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1 className="logo">OMNIGATEWAY.NET</h1>
-      <div className="user-container">
-        <p className="user">SS</p>
-        <p className="username">Steve Slavutskiy</p>
-      </div>
+      <div className="user-container"></div>
     </div>
   );
 };
