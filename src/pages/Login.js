75f4a481397dfcfe3caf012ad180f7c7c06567ae
David Orson <onlyorson@outlook.com>
2020-08-22 13:51:49 +0100

    Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import "../css/Login.css";
+
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div className="login-container">
+      <h1 className="test">Welcome, New User</h1>
+      <p>To get started, please click the button below.</p>
+      <button className="button">Get Started</button>
+    </div>
+  );
 };
 
 export default Login;
