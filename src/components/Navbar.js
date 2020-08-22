7756dd772d7f8ec26913bdd2e28fabd320c69167
David Orson <onlyorson@outlook.com>
2020-08-22 16:13:55 +0100

    Navbar Conditional Rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,26 +9,34 @@ const Navbar = (props) => {
 
   return (
     <div className="navbar">
-      <h2 className="nav-title">APPS</h2>
+      {props.mid ? <h2 className="nav-title">APPS</h2> : null}
 
       {/* <Link to={`/?mid=${props.mid}&code=${props.code}`}> */}
-      <NavLink to={`/`} className={` ${"major-link" /* inactive */}`}>
-        Clover to Shopify Product Sync
-      </NavLink>
+      {props.mid ? (
+        <NavLink to={`/`} className={` ${"major-link" /* inactive */}`}>
+          Clover to Shopify Product Sync
+        </NavLink>
+      ) : null}
+
       {/* </Link> */}
 
       {/* Strange fix to preserve the Navlink as Active for styling but have the Link redirect to mid and code params. The Navlink's Events have been muted in css */}
 
       <div className="minor-container">
-        <p className="minor-link">Mapping Rules</p>
-        <div className="sub-container">
-          <NavLink to="/products" className="sub-link">
-            For Items/Products
-          </NavLink>
-          <NavLink to="/categories" className="sub-link">
-            For Categories
-          </NavLink>
-        </div>
+        {props.mid ? (
+          <div>
+            <p className="minor-link">Mapping Rules</p>
+            <div className="sub-container">
+              <NavLink to="/products" className="sub-link">
+                For Items/Products
+              </NavLink>
+              <NavLink to="/categories" className="sub-link">
+                For Categories
+              </NavLink>
+            </div>
+          </div>
+        ) : null}
+
         {clDashboardUrl ? (
           <a href={clDashboardUrl} className="minor-link">
             Back to Clover
